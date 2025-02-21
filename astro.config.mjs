a9c6d8fccf5081e6c364a7d4892ae42bc5861c35
iwasaki <poettype73@gmail.com>
2025-02-21 13:57:05 +0900

    Add MDX

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
-import icon from 'astro-icon';
 // @ts-check
 import { defineConfig } from 'astro/config';
+
+import mdx from '@astrojs/mdx';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
@@ -13,5 +15,5 @@ export default defineConfig({
       },
     },
   },
-  integrations: [icon()],
+  integrations: [mdx(), icon()],
 });
