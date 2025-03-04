9617f3f16b8036dd3ea7024461e39a5dc970d652
iwasaki <poettype73@gmail.com>
2025-03-04 12:38:27 +0900

    Add Markdown Components

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,40 @@ import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
 import icon from 'astro-icon';
+import rehypePrettyCode from 'rehype-pretty-code';
+import { siteConfig } from './src/config';
+
+const { siteUrl } = siteConfig;
+
+const codeOptions = {
+  theme: 'catppuccin-frappe',
+  defaultLang: 'plaintext',
+  // @ts-ignore
+  onVisitLine(node) {
+    if (node.children.length === 0) {
+      node.children = [
+        {
+          type: 'text',
+          value: ' ',
+        },
+      ];
+    }
+  },
+  // @ts-ignore
+  onVisitHighlightedLine(node) {
+    if (!node.properties) {
+      node.properties = {}; // propertiesが定義されていない場合は初期化する
+    }
+    if (!node.properties.className) {
+      node.properties.className = []; // classNameが定義されていない場合は初期化する
+    }
+    node.properties.className.push('highlighted');
+  },
+};
 
 // https://astro.build/config
 export default defineConfig({
+  site: siteUrl,
   vite: {
     css: {
       preprocessorOptions: {
@@ -16,4 +47,9 @@ export default defineConfig({
     },
   },
   integrations: [mdx(), icon()],
+  markdown: {
+    syntaxHighlight: false,
+    remarkPlugins: [],
+    rehypePlugins: [[rehypePrettyCode, codeOptions]],
+  },
 });
