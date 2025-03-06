15d3380809a93df9129b9a78dbbc569861bcb152
iwasaki <poettype73@gmail.com>
2025-03-06 10:22:49 +0900

    Fix config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,20 +2,26 @@ import type { HeaderLink, SiteConfig } from '@/types/config';
 
 export const siteConfig: SiteConfig = {
   siteTitle: 'munus',
-  siteDesc: '',
+  siteDesc: '日常のつぶやき置き場',
   siteUrl: 'https://munus.dev',
   siteType: 'website',
   siteLocale: 'ja_JP',
   siteIcon: '/favicon.svg',
   siteImg: '/ogp.png',
-  background: {
-    src: 'https://placehold.jp/1300x300.png',
-  },
 };
 
 export const headerLinks: HeaderLink[] = [
   { name: 'Home', url: '/' },
-  { name: 'Tag', url: '/tag' },
+  {
+    name: 'Blog',
+    url: '',
+    popover: [
+      { name: 'Archive', url: '/archive' },
+      { name: 'Category', url: '/blog/category' },
+      { name: 'Tag', url: '/blog/tag' },
+    ],
+  },
+  { name: 'Work', url: '/work' },
   { name: 'About', url: '/about' },
   { name: 'Contact', url: '/contact' },
 ];
