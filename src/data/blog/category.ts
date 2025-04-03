68440d6451afd6d66c0eb7995755d6f6012bd698
iwasaki <poettype73@gmail.com>
2025-04-04 00:20:56 +0900

    Fix category data

--- src/data/blog/category.ts
+++ src/data/blog/category.ts
@@ -1,6 +1,6 @@
 import type { Category } from '@/types/config';
 
 export const categoryList: Category[] = [
-  { name: '書籍紹介', slug: 'book' },
+  // { name: '書籍紹介', slug: 'book' },
   { name: '雑記', slug: 'note' },
 ];
