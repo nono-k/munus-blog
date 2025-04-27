971525b4fe5a2024cf81fdf80034d7b5f80801fb
iwasaki <poettype73@gmail.com>
2025-04-27 17:45:57 +0900

    Add Tags

--- src/data/blog/tags.ts
+++ src/data/blog/tags.ts
@@ -3,5 +3,6 @@ import type { Tags } from '@/types/config';
 export const tagsList: Tags[] = [
   { name: 'ブログ', slug: 'blog' },
   { name: '美術館', slug: 'museum' },
+  { name: 'プログラミング', slug: 'programming' },
   { name: 'Markdown', slug: 'markdown' },
 ];
