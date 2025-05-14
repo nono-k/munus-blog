6b04d7bbbc898c644f4decb5daa080a00d55ad28
iwasaki <poettype73@gmail.com>
2025-05-14 09:25:47 +0900

    Add Web Tag

--- src/data/blog/tags.ts
+++ src/data/blog/tags.ts
@@ -3,6 +3,7 @@ import type { Tags } from '@/types/config';
 export const tagsList: Tags[] = [
   { name: 'ブログ', slug: 'blog' },
   { name: '美術館', slug: 'museum' },
+  { name: 'Web', slug: 'web' },
   { name: 'プログラミング', slug: 'programming' },
   { name: 'Markdown', slug: 'markdown' },
   { name: 'フォント', slug: 'font' },
