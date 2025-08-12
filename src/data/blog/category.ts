15dc32735e3b1cf117c63ccb0976577d439cece7
iwasaki <poettype73@gmail.com>
2025-08-13 01:00:07 +0900

    Add movie category

--- src/data/blog/category.ts
+++ src/data/blog/category.ts
@@ -2,6 +2,7 @@ import type { Category } from '@/types/config';
 
 export const categoryList: Category[] = [
   { name: '書籍紹介', slug: 'book' },
+  { name: '映画感想', slug: 'movie' },
   { name: '美術館', slug: 'museum' },
   { name: 'プログラミング', slug: 'programming' },
   { name: '雑記', slug: 'note' },
